6b0f5d1ae64ec9c04d416f6fd41cb1c0a6fd2ba1
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-04-13 18:20:12 -0300

    update

--- playground/example-nextjs/app/page.tsx
+++ playground/example-nextjs/app/page.tsx
@@ -1,16 +1,15 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Box, Text } from "@omariosouto/common-ui-web";
 
 
 export default function HomeScreen() {
   return (
     <Box>
-      <Avatar
+      {/* <Avatar
         className="size-12"
       >
         <AvatarImage src="https://github.com/omariosouto.png" />
         <AvatarFallback className="text-white bg-black">MS</AvatarFallback>
-      </Avatar>
+      </Avatar> */}
       <Text className="text-3xl font-bold underline">Hello World!</Text>
     </Box>
   )
