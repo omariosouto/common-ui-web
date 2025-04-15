7953535fef6284eb5d13d392deb894445600e579
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-04-15 13:08:35 -0300

    update

---
+++ playground/example-nextjs/app/async-state/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@omariosouto/common-ui-web/components";
+
+export default function Screen() {
+  return (
+    <Box>
+      <Text tag="h1">
+        This is a test screen for the async state.
+      </Text>
+    </Box>
+  );
+}
