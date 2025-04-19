8f6fcb1d4c6e066f216229e59fb59384bd7adc4c
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-04-19 09:27:12 -0300

    update

--- packages/lib/src/components/_base/accordion.tsx
+++ packages/lib/src/components/_base/accordion.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDownIcon } from "lucide-react"
 
-import { cn } from "../../infra/utils"
+import { cn } from "./infra/utils"
 
 function Accordion({
   ...props
--- packages/lib/src/components/_base/avatar.tsx
+++ packages/lib/src/components/_base/avatar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
-import { cn } from "../../infra/utils";
+import { cn } from "./infra/utils";
 
 function Avatar({
   className,
