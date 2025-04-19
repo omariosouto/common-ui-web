d38423581f2a18a64e43c849dd34bb5218f60488
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-04-19 11:35:22 -0300

    update

--- packages/lib/src/components/_primal/InputText/InputTextPrimal.tsx
+++ packages/lib/src/components/_primal/InputText/InputTextPrimal.tsx
@@ -21,7 +21,9 @@ export function InputTextPrimal({
         "border-2 border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-foreground",
         className,
       )}
-      onChange={onChange}
+      onChange={(e) => {
+        onChange?.(e);
+      }}
       defaultValue={defaultValue}
       {...props}
     />
